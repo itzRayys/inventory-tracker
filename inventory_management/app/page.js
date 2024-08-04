c5a48e76a7c18fc1345f85104472127991cfb25e
Noah <pancakeboy55@gmail.com>
2024-08-04 12:04:43 -0500

    Updated Visuals

--- inventory_management/app/page.js
+++ inventory_management/app/page.js
@@ -70,7 +70,7 @@ export default function Home() {
   const handleClose = () => setOpen(false)
 
   return(
-    <Box width={'100vw'} height={'100vh'} display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'} gap={2}>
+    <Box width={'100vw'} height={'100vh'} display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'} gap={2} bgcolor={'#0f111a'}>
       <Modal 
       open={open} 
       onClose={handleClose}>
@@ -108,18 +108,18 @@ export default function Home() {
           </Stack>
         </Box>
       </Modal>
-      <Box border={'1px solid #000000'} bgcolor={'white'} borderRadius={2}>
-        <Box width={'85vw'} height='100px' bgcolor='#ADD8E6' display={'flex'} justifyContent={'space-between'} borderRadius={2} alignItems={'center'} p={5} pr={10}>
-          <Typography variant="h2" color='#333'>Inventory Items</Typography>
+      <Box border={'5px solid #1e253b'} bgcolor={'#0f111a'} boxShadow={'0px 0px 20px #1e253b'}>
+        <Box width={'85vw'} height='100px' bgcolor='#1c2336' display={'flex'} justifyContent={'space-between'} alignItems={'center'} p={5} pr={10}>
+          <Typography variant="h2" color='#c1c9e3' fontFamily={'Roboto'}>Inventory Items</Typography>
       <Button variant="contained" onClick={() => {
         handleOpen()
       }}>Add New Item</Button>
         </Box>
-        <Stack width='85vw' height='600px' spacing={2} overflow={'auto'}>
+        <Stack width='85vw' height='600px' spacing={2} overflow={'auto'} p={2}>
           {
             inventory.map(({name, quantity}) => (
-              <Box key={name} width={'100%'} minHeight={'150px'} display={'flex'} justifyContent={'space-between'} alignItems={'center'} borderRadius={2} bgcolor={'#f0f0f0'} padding={5}>
-                <Typography variant="h3" color={'#333'} textAlign={'center'}>{name.charAt(0).toUpperCase() + name.slice(1)}</Typography>
+              <Box key={name} width={'100%'} minHeight={'150px'} display={'flex'} justifyContent={'space-between'} alignItems={'center'} borderRadius={2} bgcolor={'#1c2336'} padding={5} boxShadow={'0px 0px 10px #132661'}>
+                <Typography variant="h3" color={'#c1c9e3'} textAlign={'center'}>{name.charAt(0).toUpperCase() + name.slice(1)}</Typography>
                 <Stack direction={'row'} spacing={5}>
                   <Button variant="outlined" onClick={() => {
                     removeItem(name)
@@ -128,7 +128,7 @@ export default function Home() {
                     fontSize: 18,
                   }}
                   >-</Button>
-                  <Typography variant="h3" color={'#333'} textAlign={'center'}>{quantity}</Typography>
+                  <Typography variant="h3" color={'#c1c9e3'} textAlign={'center'}>{quantity}</Typography>
                   <Button variant="outlined" onClick={() => {
                     addItem(name)
                   }}
